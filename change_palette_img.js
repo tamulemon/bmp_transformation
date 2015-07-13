499e310876ae5605fda3afd030302a418f489b1a
Meng Chen <tamulemon@gmail.com>
2015-07-13 12:45:27 -0700

    change readInt8 to readUInt8

--- change_palette_img.js
+++ change_palette_img.js
@@ -11,16 +11,23 @@ fs.readFile('./palette-bitmap.bmp', function(err, data) {
 	imageSpecs.imageHeight = data.readUInt32LE(22); 
 	imageSpecs.colorDepth = data.readUInt32LE(28); 
 	imageSpecs.numColorInPalette = data.readUInt32LE(46);
+	imageSpecs.paletteColor1 = [data.readUInt8(54), data.readUInt8(55),data.readUInt8(56),data.readUInt8(57)];
+	imageSpecs.paletteColor28 = [data.readUInt8(54+27*4), data.readUInt8(54+27*4+1), data.readUInt8(54+27*4+2), data.readUInt8(54+27*4+3)];
+	imageSpecs.paletteColor256 = [data.readUInt8(54+255*4), data.readUInt8(54+255*4+1), data.readUInt8(54+255*4+2), data.readUInt8(54+255*4+3)];
 	imageSpecs.bufferSize = data.length;
-
+	imageSpecs.pixel1 = data.readInt8(imageSpecs.imageStart);
 	console.log(imageSpecs);
 	
+	// swap color changes for all the colors stored in palette
 	for ( var i = 54; i < imageSpecs.imageStart; i += 4 ) {
 		// swapping the blue and red channels
 		var temp3 = data[i];
 		data[i] = data[i+2];
 		data[i+2] = temp3;
 	}
+	
+	//change the pointer of each pixel
+	
 	 // call the write buffer function
 	writeBuff(data);
 	
